0c62c1001da129dad7ff1553d5f801240de57806
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-05-23 23:36:45 -0300

    Created Initial Backend Project

---
+++ Book-Tracker-Backend/src/test/java/com/example/booktrackerbackend/BookTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booktrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerBackendApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
