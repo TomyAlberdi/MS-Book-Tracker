408b46e927a454f5a906b21c0b69a4f3b4a14122
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2025-06-01 18:58:03 -0300

    started register page design

--- Book-Tracker-Frontend/src/components/Auth/Register.tsx
+++ Book-Tracker-Frontend/src/components/Auth/Register.tsx
@@ -1,6 +1,16 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="h-screen w-full flex justify-center items-center">
+      <Card className="w-1/3">
+        <CardHeader>
+          <CardTitle>
+
+          </CardTitle>
+        </CardHeader>
+      </Card>
+    </div>
   )
 }
 export default Register
