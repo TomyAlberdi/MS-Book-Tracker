dedfa64e33e9f30c39fa3d619a189ae4ab883015
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2025-06-01 21:05:46 -0300

    Implemented mockup register and login pages + forms

--- Book-Tracker-Frontend/src/components/Auth/Login.tsx
+++ Book-Tracker-Frontend/src/components/Auth/Login.tsx
@@ -1,6 +1,91 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  username: z.string().min(3, "Username must be at least 3 characters long"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
 const Login = () => {
+  const { t } = useTranslation();
+
+  const [LoadingRequest, setLoadingRequest] = useState(false)
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    //TODO: Implement API request on Login using context
+    console.log(data)
+  };
+
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <div className="h-screen w-full flex justify-center items-center">
+      <Card className="w-1/3">
+        <CardHeader>
+          <CardTitle>{t("Login")}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("Username")}</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("Password")}</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className="w-full" disabled={LoadingRequest}>
+                {t("Login")}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+export default Login;
--- Book-Tracker-Frontend/src/components/Auth/Register.tsx
+++ Book-Tracker-Frontend/src/components/Auth/Register.tsx
@@ -1,16 +1,93 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  username: z.string().min(3, "Username must be at least 3 characters long"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
 
 const Register = () => {
+  const { t } = useTranslation();
+
+  const [LoadingRequest, setLoadingRequest] = useState(false)
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    //TODO: Implement API request on register using context
+    console.log(data)
+  };
+
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <Card className="w-1/3">
         <CardHeader>
-          <CardTitle>
-
-          </CardTitle>
+          <CardTitle>{t("Register")}</CardTitle>
+          <CardDescription>{t("CreateAccount")}</CardDescription>
         </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("Username")}</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t("Password")}</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className="w-full" disabled={LoadingRequest}>
+                {t("Register")}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
       </Card>
     </div>
-  )
-}
-export default Register
+  );
+};
+export default Register;
