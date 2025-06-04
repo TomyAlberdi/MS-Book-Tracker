5f1c3e3735f42061895e1669ca9ca006f7d92869
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-06-04 19:00:09 -0300

    created book interfaces

--- Book-Tracker-Frontend/src/lib/interfaces.tsx
+++ Book-Tracker-Frontend/src/lib/interfaces.tsx
@@ -7,3 +7,22 @@ export interface UserWithCredentials {
   username: string;
   password: string;
 }
+
+export interface PartialBook {
+  id: string;
+  volumeInfo: {
+    title: string;
+    authors: string[];
+    publishedDate: string;
+    imageLinks: {
+      smallThumbnail: string;
+      thumbnail: string;
+    }
+    language: string;
+  }
+}
+
+export interface CompleteBook extends PartialBook {
+  pageCount: number;
+  maturityRating: string;
+}
