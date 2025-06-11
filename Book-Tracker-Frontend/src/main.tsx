0c0d07713a452b626dca63488de70c100d7fea52
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-06-11 18:59:46 -0300

    removed react strict mode

--- Book-Tracker-Frontend/src/main.tsx
+++ Book-Tracker-Frontend/src/main.tsx
@@ -4,20 +4,17 @@ import BookContextComponent from "@/context/BookContextComponent";
 import UserContextComponent from "@/context/UserContextComponent";
 import "@/i18n";
 import "@/index.css";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <BrowserRouter>
-        <UserContextComponent>
-          <BookContextComponent>
-            <App />
-          </BookContextComponent>
-        </UserContextComponent>
-      </BrowserRouter>
-    </ThemeProvider>
-  </StrictMode>
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <BrowserRouter>
+      <UserContextComponent>
+        <BookContextComponent>
+          <App />
+        </BookContextComponent>
+      </UserContextComponent>
+    </BrowserRouter>
+  </ThemeProvider>
 );
